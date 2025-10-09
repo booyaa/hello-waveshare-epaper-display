33366d832205d6a7f1e8b97e60aae3ecd8566c46
Mark Sta Ana <mark.sta.ana@smartpension.co.uk>
2025-10-09 16:06:01 +0100

    refactor: to use helper and shared code

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import os
 import logging
 import time
 from PIL import Image,ImageDraw,ImageFont
+import helpers
 
 picdir = os.path.join((os.path.dirname(os.path.realpath(__file__))), 'lib', 'waveshare-epd', 'RaspberryPi_JetsonNano', 'python','pic')
 libdir = os.path.join((os.path.dirname(os.path.realpath(__file__))), 'lib', 'waveshare-epd', 'RaspberryPi_JetsonNano', 'python', 'lib')
@@ -17,8 +18,16 @@ from waveshare_epd import epd2in13b_V4 # pyright: ignore[reportMissingImports] #
 logging.basicConfig(level=logging.DEBUG)
 
 def main():
+    module_name = "main"
+    script_to_import = "hello_world.py"
+    module = helpers.load_module_safely(module_name, script_to_import)
+
+    if not hasattr(module, 'main'):
+        print("The module must have a 'main' function.")
+        sys.exit(1)
+
     try:
-        font = ImageFont.truetype(os.path.join(picdir, 'Font.ttc'), 20)
+        # font = ImageFont.truetype(os.path.join(picdir, 'Font.ttc'), 20)
 
         logging.info("Initialize the display")
         epd = epd2in13b_V4.EPD()
@@ -26,9 +35,10 @@ def main():
         epd.Clear()
 
         logging.info("Create a new bitmap")
-        image = Image.new('1', (250, 122), 255)  # 255: clear the frame
-        draw = ImageDraw.Draw(image)
-        draw.text((10, 10), "Hello, World!", font=font, fill=0)
+        # image = Image.new('1', (250, 122), 255)  # 255: clear the frame
+        # draw = ImageDraw.Draw(image)
+        # draw.text((10, 10), "Hello, World!", font=font, fill=0)
+        image = module.main()
 
         logging.info("Push the bitmap to the display")
         epd.display(epd.getbuffer(image),epd.getbuffer(image))
