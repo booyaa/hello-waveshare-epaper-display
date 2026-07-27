6ee36c3de3a2188d380ae487ed3bfb7d2c6d98f2
booyaa <booyaabooyaabooyaa@gmail.com>
2026-07-27 13:11:34 +0100

    feat: custom messages

---
+++ _pillow_display_text.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+
+"""
+Send text to paper display
+"""
+
+import os
+from PIL import Image, ImageDraw, ImageFont
+
+def get_image(text="Hello, World!"):
+    picdir = os.path.join((os.path.dirname(os.path.realpath(__file__))), 'lib', 'waveshare-epd', 'RaspberryPi_JetsonNano', 'python','pic')
+
+    font = ImageFont.truetype(os.path.join(picdir, 'Font.ttc'), 40)
+    image = Image.new('1', (250, 122), 255)
+    draw = ImageDraw.Draw(image)
+    # display text in the centre of the image (250 pixels wide, 122 pixels high)
+    bbox = draw.textbbox((0, 0), text, font=font)
+    w, h = bbox[2] - bbox[0], bbox[3] - bbox[1]
+    draw.text(((250 - w) / 2, (122 - h) / 2), text, font=font, fill=0)
+    
+    return image
+
+if __name__ == "__main__":
+    print("Don't run this directly test using pillow_fight.py")
---
+++ display_text.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+import sys
+import os
+import logging
+import time
+import helpers
+
+picdir = os.path.join((os.path.dirname(os.path.realpath(__file__))), 'lib', 'waveshare-epd', 'RaspberryPi_JetsonNano', 'python','pic')
+libdir = os.path.join((os.path.dirname(os.path.realpath(__file__))), 'lib', 'waveshare-epd', 'RaspberryPi_JetsonNano', 'python', 'lib')
+
+if os.path.exists(libdir):
+    sys.path.append(libdir)
+
+
+# DEBUG can be noisy
+logging.basicConfig(level=logging.WARNING)
+
+def main():
+    from waveshare_epd import epd2in13b_V4 # pyright: ignore[reportMissingImports] # added manually (see above)
+    module_name = "get_image"
+    this_script_file_name = os.path.basename(__file__)
+    script_to_import = f"_pillow_{this_script_file_name}"
+    if not os.path.isfile(script_to_import):
+        exit(f"Error: {script_to_import} not found.")
+
+    module = helpers.load_module_safely(module_name, script_to_import)
+    params = sys.argv[1:] # text to send
+
+    if not hasattr(module, module_name):
+        print(f"The module must have a '{module_name}' function.")
+        sys.exit(1)
+
+    try:
+        logging.info("Initialize the display")
+        epd = epd2in13b_V4.EPD()
+        epd.init()
+        epd.Clear()
+
+        logging.info("Create a new bitmap")
+        image = module.get_image(*params)  # FIXME: refactor as a class using https://www.danielmorell.com/blog/dynamically-calling-functions-in-python-safely
+
+        logging.info("Push the bitmap to the display")
+
+        # how to make the image black instead of red
+        from PIL import Image, ImageDraw, ImageFont
+        image2 = Image.new('1', (250, 122), 255) # create blank 1-bit white image
+
+        image = image.rotate(180) # display orientation is wrong when in box
+        epd.display(epd.getbuffer(image),epd.getbuffer(image2)) # black, red
+
+        logging.info("Goto Sleep...")
+        epd.sleep()
+        time.sleep(3)
+
+    except IOError as e:
+        logging.info(e)
+
+    except KeyboardInterrupt:
+        logging.info("ctrl + c:")
+        epd2in13b_V4.epdconfig.module_exit(cleanup=True)
+        exit()
+
+
+if __name__ == "__main__":
+    main()
